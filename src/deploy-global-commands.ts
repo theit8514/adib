3c9e1290f9f44263eb6d010aefc201e577fa14ee
theit8514 <theit8514@users.noreply.github.com>
2022-01-09 02:06:25 -0500

    Add deploy global commands

---
+++ src/deploy-global-commands.ts
@@ -0,0 +1,20 @@
+import fs from 'fs'
+
+import discordConfig from './discord-config.js'
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v9'
+import { ICommand } from './icommand.js';
+
+const rest = new REST({ version: '9' }).setToken(discordConfig.TOKEN);
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+const commands = await Promise.all(commandFiles.map(async file => {
+    const commandImport = await import(`./commands/${file}`);
+    const command = commandImport.default as ICommand;
+    return command.data.toJSON();
+}));
+
+rest.put(Routes.applicationCommands(discordConfig.CLIENT_ID), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
